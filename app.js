de8ebe903f26e228b1431996ea1591b0ac82ae47
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-10-01 00:07:55 -0400

    added dom elements and event listener

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 // // import functions and grab DOM elements
-// - Shoot button
-// - Reset button
 
-HTML
-// - Rock radio button 
-// - Scissors radio  button
-// - Paper radio button
+const shootButton = document.getElementById('shoot-button');
+const resetButton = document.getElementById('reset-button');
+const winDiv = document.getElementById('win-div');
+const lossDiv = document.getElementById('loss-div');
+const drawDiv = document.getElementById('draw-div')
+const totalDiv = document.getElementById('totalDiv');
 
+let wins = 0;
+let losses= 0;
+let draws = 0;
+let total = 0;
 
+shootButton.addEventListener('click', () => {
+
+    const selectedChoice = document.querySelector('input[radio]:checked');
+
+    let userShoot = selectedChoice.value;
+    let cpuShoot = 'scissors';
+
+    // if (Math.random() < .3) {
+    //     cpuShoot = 'paper'
+    //     cpuShoot = 'rock'
+    // }
+})
 // Span
 //     -Wins
 //         -textcontent
